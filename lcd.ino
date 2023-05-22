bc502a168210b0d63f347ab3c53f4a127df14eaa
Muhammad Mahmoud <87485763+skipcl4ss@users.noreply.github.com>
2023-05-22 21:23:08 +0300

    Update lcd.ino

--- lcd.ino
+++ lcd.ino
@@ -1,30 +1,34 @@
-// Variables for the value of the moisture and its percentage
-float val;
-float valPercent;
-// Saving the moisture sensor pin number in a variable
-int moistPin = 5;
 #include <LiquidCrystal.h>
 LiquidCrystal LCD(12, 11, 10, 9, 8, 7);
+#define power A1
+#define moistPin A0
+int moisture;
+float moisturePercentage;
 
 void setup() {
-    // put your setup code here, to run once:
+  // put your setup code here, to run once:
+    pinMode(power, OUTPUT);
+    pinMode(moistPin, INPUT);
     Serial.begin(9600);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    val = analogRead(moistPin);
-    valPercent = 100 * val / 1023;
-    // Print the value and its corresponding percentage to the serial monitor
-    Serial.print("val: ");
-    Serial.println(int(val));
-    Serial.print("valPercent: ");
-    Serial.println(valPercent);
-    // Print the value and its corresponding percentage to the LCD screen
-    LCD.setCursor(1,0);
+    // Apply power to the soil moisture sensor
+    digitalWrite(power, HIGH);
+    delay(10); // Wait for 10 millisecond(s)
+    moisture = analogRead(moistPin);
+    moisturePercentage = (float) 100 * moisture / 1023;
+    // Turn off the sensor to reduce metal corrosion over time
+    digitalWrite(power, LOW);
+    Serial.print("moisture: ");
+    Serial.println(moisture);
+    Serial.print("moisturePercentage: ");
+    Serial.println(moisturePercentage);
+    LCD.setCursor(1, 0);
     LCD.print("Moisture: ");
-    LCD.print(int(valPercent));
+    LCD.print(int(moisturePercentage));
     LCD.print("%");
-    delay(1000);
+    // Wait for 100 millisecond(s)
+    delay(100);
     LCD.clear();
 }
